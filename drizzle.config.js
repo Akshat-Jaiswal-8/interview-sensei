a852f40b55905eb704236a9e8e6c8e5ea663e450
Akshat Jaiswal <akshatjaiswal.official@gmail.com>
2025-05-29 10:55:44 +0530

    fix: removed the exposed db link.

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
   schema: "./utils/schema.js",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://interviewDb_owner:npg_DZX2zhFB5NIS@ep-wispy-moon-a5rwhfmm-pooler.us-east-2.aws.neon.tech/interviewDb?sslmode=require",
+    url: process.env.DATABASE_URL,
   },
 };
